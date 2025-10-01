b748fde716c4f2c7a3763e79a93f8ae9d882e4eb
Akshaya7845 <akshaya.murugan@psiog.com>
2025-10-01 14:03:08 +0530

    dynamic type example

--- sectionA/part4.cs
+++ sectionA/part4.cs
@@ -1,5 +1,5 @@
 // var example
-using System;
+/*using System;
 class Program
 {
     static void Main()
@@ -9,4 +9,27 @@ class Program
         Console.WriteLine("Name: " + name);
         Console.WriteLine("Type of 'name': " + name.GetType());  
     }
+}*/
+//dynamic example:
+using System;
+class Program
+{
+    static void Main()
+    {
+        dynamic item = "Hello";
+        Console.WriteLine("item (string): " + item);
+
+        item = 123;
+        Console.WriteLine("item (int): " + item);
+
+        item = true;
+        Console.WriteLine("item (bool): " + item);
+
+        var x = "hello";
+        Console.WriteLine("x.Length: " + x.Length);
+
+        dynamic y = "world";
+        Console.WriteLine("y.Length: " + y.Length);
+    }
 }
+
