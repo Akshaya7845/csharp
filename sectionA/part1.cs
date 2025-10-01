f1cbc66283756615f8f45389cde1bf87ddd260f6
Akshaya7845 <akshaya.murugan@psiog.com>
2025-10-01 10:51:22 +0530

    conditional applied

--- sectionA/part1.cs
+++ sectionA/part1.cs
@@ -14,7 +14,7 @@ class Program
 }*/
 // undef example:
 
-#define TEST      
+/*#define TEST      
 #undef TEST       
 
 using System;
@@ -27,5 +27,43 @@ class Program
         Console.WriteLine("This won't be printed");
 #endif
     }
+}*/
+
+//#if, #elif, #else, #endif – Conditional Compilation: 
+/*#define RELEASE
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+#if DEBUG
+        Console.WriteLine("Debug build");
+#elif RELEASE
+        Console.WriteLine("Release build");
+#else
+        Console.WriteLine("Unknown build");
+#endif
+    }
+}*/
+//#region and #endregion
+#region StudentDetails
+class Student
+{
+    public string name = "Akshaya";
+    public int age = 22;
 }
+
+#endregion
+class Program
+{
+    static void Main()
+    {
+        Student s = new Student();
+        Console.WriteLine($"Name: {s.name}, Age: {s.age}");
+    }
+}
+
+
 
