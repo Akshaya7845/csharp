bd13e38da580dd67fc12dc1a9ebfb66a3c7c8eaf
Akshaya7845 <akshaya.murugan@psiog.com>
2025-10-01 13:57:42 +0530

    var type example

---
+++ sectionA/part4.cs
@@ -0,0 +1,12 @@
+// var example
+using System;
+class Program
+{
+    static void Main()
+    {
+        var name = "Kumar";  
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Type of 'name': " + name.GetType());  
+    }
+}
