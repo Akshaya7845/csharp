6e13db3d4c4ea38ca142e340a4b8a7c6eea36079
Akshaya7845 <akshaya.murugan@psiog.com>
2025-10-03 00:00:51 +0530

    constructor chaining example

--- sectionB/part5.cs
+++ sectionB/part5.cs
@@ -23,7 +23,7 @@ namespace StudentApp
 }
 */
 //constructor example
-using System;
+/*using System;
 
 namespace StudentApp
 {
@@ -44,6 +44,66 @@ namespace StudentApp
         {
             Student s1 = new Student();
             Console.WriteLine(s1.Name); 
+        }
+    }
+}
+*/
+//constructor chaining:
+using System;
+
+namespace StudentApp
+{
+    class Student
+    {
+        public string Name;
+        public int Age;
+
+        // default constructor
+        public Student() : this("No Name", 0)
+        {
+            Console.WriteLine("default constructor called.");
+        }
+
+        // Constructor that accepts only name, sets default age
+        public Student(string name) : this(name, 0)
+        {
+            Console.WriteLine("constructor with only name called.");
+        }
+
+        // Constructor that accepts name and age
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            Console.WriteLine("parameterized constructor called.");
+        }
+
+        // Method to display student info
+        public void Display()
+        {
+            Console.WriteLine($"Student Name: {Name}, Age: {Age}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  student using default constructor
+            Student s1 = new Student();
+            s1.Display(); 
+
+            Console.WriteLine();
+
+            //  student with name only
+            Student s2 = new Student("Akshaya");
+            s2.Display(); 
+
+            Console.WriteLine();
+
+            //  student with name and age
+            Student s3 = new Student("Aishwarya", 24);
+            s3.Display(); 
         }
     }
 }
