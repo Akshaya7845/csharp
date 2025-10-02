89ffd4f35229838adb7b49746f52c5dd4a5df325
Akshaya7845 <akshaya.murugan@psiog.com>
2025-10-03 00:26:20 +0530

    access modifiers-private

--- sectionB/part8.cs
+++ sectionB/part8.cs
@@ -1,6 +1,6 @@
 //aceess modifiers
 //public
-using System;
+/*using System;
 
 class Demo
 {
@@ -13,5 +13,28 @@ class Program
     {
         Demo d = new Demo();
         Console.WriteLine(d.name); 
+    }
+}
+*/
+//private
+using System;
+
+class Demo
+{
+    private string secret = "Hidden";
+    public void Show()
+    {
+        Console.WriteLine(secret);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Demo d = new Demo();
+
+        // access private field using a public method
+        d.Show();  
     }
 }
