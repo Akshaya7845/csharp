badc59fc088b07f70977c21e44f8113cb5554ea4
Akshaya7845 <akshaya.murugan@psiog.com>
2025-10-05 22:27:42 +0530

    task/tpl example

--- sectionG/part33.cs
+++ sectionG/part33.cs
@@ -1,5 +1,5 @@
 //thread example:
-using System;
+/*using System;
 using System.Threading;
 
 class ATMExample
@@ -23,5 +23,44 @@ class ATMExample
         // Wait for fraud check to finish
         fraudThread.Join();
         Console.WriteLine("transaction completed.");
+    }
+}
+*/
+//task/tpl example:
+using System;
+using System.Threading.Tasks;
+class FoodDeliveryApp
+{
+    static async Task PlaceOrderAsync()
+    {
+        Console.WriteLine("order placed");
+        await Task.Delay(1000);
+        var restaurantTask = NotifyRestaurantAsync();
+        var paymentTask = VerifyPaymentAsync();
+        var mapTask = UpdateMapAsync();
+        await Task.WhenAll(restaurantTask, paymentTask, mapTask);
+        Console.WriteLine("order confirmed and being prepared.");
+    }
+
+    static async Task NotifyRestaurantAsync()
+    {
+        await Task.Delay(1500);
+        Console.WriteLine(" restaurant notified");
+    }
+    static async Task VerifyPaymentAsync()
+    {
+        await Task.Delay(1000);
+        Console.WriteLine("payment Verified");
+    }
+    static async Task UpdateMapAsync()
+    {
+        await Task.Delay(800);
+        Console.WriteLine("map Updated with delivery Route");
+    }
+    static async Task Main()
+    {
+        Console.WriteLine("opening Food Delivery App...");
+        await PlaceOrderAsync();
+        Console.WriteLine("rider Assigned. order is on the way!");
     }
 }
