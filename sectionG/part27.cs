b806ece88fae7317fb92f436e798623406a64b1b
Akshaya7845 <akshaya.murugan@psiog.com>
2025-10-05 14:34:49 +0530

    singleton using thread

--- sectionG/part27.cs
+++ sectionG/part27.cs
@@ -1,5 +1,5 @@
 //thread safe singleton class
-using System;
+/*using System;
 
 public class ATMSystem
 {
@@ -48,6 +48,62 @@ class Program
         Console.WriteLine("\nSame instance? " + Object.ReferenceEquals(atm1, atm2)); // True
 
         Console.WriteLine("\nTest completed.");
+    }
+}
+*/
+//singleton with threads:
+using System;
+using System.Threading;
+public class ATMSystem
+{
+    private static ATMSystem _instance;
+    private static readonly object _lock = new object();
+
+    private ATMSystem()
+    {
+        Console.WriteLine("ATM system started");
+    }
+
+    public static ATMSystem Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                        _instance = new ATMSystem();
+                }
+            }
+            return _instance;
+        }
+    }
+    public void ProcessTransaction(string user)
+    {
+        Console.WriteLine($"Transaction processed for {user} | ATM Instance: {this.GetHashCode()}");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("transactions using threads \n");
+
+        // start 5 threads (simulating 5 users)
+        for (int i = 1; i <= 5; i++)
+        {
+            int userId = i; // capture loop variable
+            Thread thread = new Thread(() =>
+            {
+                ATMSystem.Instance.ProcessTransaction($"User {userId}");
+            });
+            thread.Start();
+        }
+        // wait to ensure all threads finish
+        Thread.Sleep(1000);
+
+        Console.WriteLine("\n transactions completed.");
     }
 }
 
